a5e1ab50c839f3a732b2c29728c3d7b72f690a95
Creator149312 <gauravsingh9314@gmail.com>
2024-07-29 11:19:12 +0530

    email verification page UI updated

--- app/verify/new-email/page.js
+++ app/verify/new-email/page.js
@@ -1,7 +1,9 @@
+"use client"
+import { Suspense } from "react";
 import AccountEmailVerification from "@components/AccountEmailVerification";
 
 const page = () => {
-  return <AccountEmailVerification />;
+  return <Suspense><AccountEmailVerification /></Suspense>;
 };
 
 export default page;
