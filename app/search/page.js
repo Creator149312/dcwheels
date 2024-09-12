8f0697675a1317119d370d63a66dcfa0def9020f
Creator149312 <gauravsingh9314@gmail.com>
2024-09-12 09:11:44 +0530

    Search wheels page updated with popular wheels link and sitemap updated to index popular wheels

--- app/search/page.js
+++ app/search/page.js
@@ -1,29 +1,17 @@
-// import ListDisplay from "@components/ListDisplay";
-import RemoveListBtn from "@components/RemoveListBtn";
 import SearchBarNav from "@components/SearchNavBar";
-import WheelWithInput from "@components/WheelWithInput";
 import { Button } from "@components/ui/button";
 import { Card } from "@components/ui/card";
-import { connectMongoDB } from "@lib/mongodb";
-import Wheel from "@models/wheel";
-import { validateObjectID } from "@utils/Validator";
-import Link from "next/link";
-import { HiOutlineEye, HiPencilAlt } from "react-icons/hi";
-
-let titleStr = "";
-let listerror = null;
-let wordsList = null;
 
 export const metadata = {
   title: "Find Spin Wheels",
-  description:
-    "Find the spin wheels you are looking for",
+  description: "Find the spin wheels you are looking for",
 };
 
 export default function Page() {
   return (
     <>
       <div className="bg-card text-card-foreground w-full">
+        <h1 className="text-4xl m-2 flex justify-center items-center">Find Spin Wheels</h1>
         <SearchBarNav />
         <div className="text-lg pt-2 flex justify-center items-center">
           You can create your own wheels by{" "}
@@ -32,6 +20,98 @@ export default function Page() {
               Creating an Account
             </Button>
           </a>
+        </div>
+        <h2 className="text-2xl mt-4 mb-2 flex justify-center items-center">
+          Popular Wheels
+        </h2>
+        <div className="grid lg:grid-cols-12 gap-x-2 mt-2 min-h-screen">
+          <div className="rounded-xl bg-card mb-2 text-card-foreground grid text-center lg:col-span-6 shadow border">
+            <ul className="m-2 text-blue-700">
+              <li className="m-2 p-2">
+                <a href="/wheels/nfl-wheel" className="text-lg">
+                  NFL Wheel
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/countries-wheel" className="text-lg ">
+                  Countries Wheel
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/emoji-wheel" className="text-lg ">
+                  Emoji Wheel
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/emotions-wheel" className="text-lg ">
+                  Emotions Wheel
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/gender-wheel" className="text-lg ">
+                  Gender Wheel
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/dice-wheel" className="text-lg ">
+                  Dice Wheel
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/animal-wheel" className="text-lg ">
+                  Animal Wheel
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/months-wheel" className="text-lg ">
+                  Months Wheel
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="rounded-xl border bg-card text-card-foreground shadow text-center lg:col-span-6">
+            <ul className="m-2 text-blue-700">
+              <li className="m-2 p-2">
+                <a href="/wheels/alphabet-wheel" className="text-lg ">
+                  Alphabet Wheel
+                </a>
+              </li>
+               <li className="m-2 p-2">
+                <a href="/wheels/truth-dare-wheel" className="text-lg ">
+                  Truth and Dare Wheel
+                </a>
+              </li> <li className="m-2 p-2">
+                <a href="/wheels/pokemon-wheel" className="text-lg ">
+                  Pokemon Wheel
+                </a>
+                </li>
+             <li className="m-2 p-2">
+                <a href="/wheels/zodiac-wheel" className="text-lg ">
+                  Zodiac Wheel
+                </a>
+              </li>
+             <li className="m-2 p-2">
+                <a href="/wheels/random-number-wheel" className="text-lg ">
+                  Random Number Wheel - 1 to 100
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/fruit-wheel" className="text-lg ">
+                Fruit Wheel
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/height-wheel" className="text-lg ">
+                Height Wheel
+                </a>
+              </li>
+              <li className="m-2 p-2">
+                <a href="/wheels/personality-wheel" className="text-lg ">
+                Personality Wheel
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
