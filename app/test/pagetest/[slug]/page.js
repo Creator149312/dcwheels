896b1621eec7be049c901c2d11e4ec64009d8d45
Creator149312 <gauravsingh9314@gmail.com>
2025-02-25 20:33:38 +0530

    Fixed Scrollablesegments errors in advanced Editor

--- app/test/pagetest/[slug]/page.js
+++ app/test/pagetest/[slug]/page.js
@@ -28,49 +28,49 @@ export default async function Home({ params }) {
 
   if (pageData === undefined) redirect("/");
 
-  return (
-    <div className="p-3">
-      {/* <WheelWithInput newSegments={segmentsData} /> */}
-      {/* <WheelWithInputContentEditable
-        newSegments={ensureArrayOfObjects(pageData.segments)}
-      /> */}
+  return (<></>
+    // <div className="p-3">
+    //   {/* <WheelWithInput newSegments={segmentsData} /> */}
+    //   {/* <WheelWithInputContentEditable
+    //     newSegments={ensureArrayOfObjects(pageData.segments)}
+    //   /> */}
 
-      <WheelWithInputContentEditable
-        newSegments={ensureArrayOfObjects(pageData.wheel.data)}
-      />
-      <h1 className="text-3xl mb-2">{pageData.title}</h1>
+    //   <WheelWithInputContentEditable
+    //     newSegments={ensureArrayOfObjects(pageData.wheel.data)}
+    //   />
+    //   <h1 className="text-3xl mb-2">{pageData.title}</h1>
 
-      {/* Map through the content and render accordingly */}
-      <div className="text-lg">
-        {pageData.content.map((item, index) => {
-          switch (item.type) {
-            case "paragraph":
-              return (
-                <p key={index} className="mb-3">
-                  {item.text}
-                </p>
-              );
-            case "image":
-              return <img key={index} src={item.src} alt={item.alt} />;
-            case "link":
-              return (
-                <a
-                  key={index}
-                  href={item.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {item.text}
-                </a>
-              );
-            case "heading":
-              const HeadingTag = `h${item.level}`; // Dynamically render the heading tag (e.g., h2, h3)
-              return <HeadingTag key={index}>{item.text}</HeadingTag>;
-            default:
-              return null;
-          }
-        })}
-      </div>
-    </div>
+    //   {/* Map through the content and render accordingly */}
+    //   <div className="text-lg">
+    //     {pageData.content.map((item, index) => {
+    //       switch (item.type) {
+    //         case "paragraph":
+    //           return (
+    //             <p key={index} className="mb-3">
+    //               {item.text}
+    //             </p>
+    //           );
+    //         case "image":
+    //           return <img key={index} src={item.src} alt={item.alt} />;
+    //         case "link":
+    //           return (
+    //             <a
+    //               key={index}
+    //               href={item.href}
+    //               target="_blank"
+    //               rel="noopener noreferrer"
+    //             >
+    //               {item.text}
+    //             </a>
+    //           );
+    //         case "heading":
+    //           const HeadingTag = `h${item.level}`; // Dynamically render the heading tag (e.g., h2, h3)
+    //           return <HeadingTag key={index}>{item.text}</HeadingTag>;
+    //         default:
+    //           return null;
+    //       }
+    //     })}
+    //   </div>
+    // </div>
   );
 }
